e069f0e647333dd103226110f7239f027d840fea
theOldSheep <sunnymengyangliu@gmail.com>
2024-08-08 18:39:59 -0600

    misc bugfix, optimization and tweaks

--- [绵羊服]泰拉瑞亚RPG灾厄服务端/server/server_start.bat
+++ [绵羊服]泰拉瑞亚RPG灾厄服务端/server/server_start.bat
@@ -4,7 +4,7 @@ title 泰拉瑞亚灾厄服务器
 :StartServer
 echo [%time%] 正在启动服务器...
 echo [%time%] Starting Server...
-"C:\Program Files\Java\jdk1.8.0_331\jre\bin\java.exe" -Xmx8g -Xms8g -jar -jar paper-1.12.2-1620.jar nogui
+"C:\Program Files\Java\jdk1.8.0_331\jre\bin\java.exe" -Xmx8g -Xms8g -jar paper-1.12.2-1620.jar nogui
 echo [%time%] 若要关闭服务器，请在倒计时结束前关闭此窗口以免数据损坏。
 echo [%time%] If you want to stop the server, close this tab before the countdown finishes to prevent data corruption.
 TIMEOUT /T 10
