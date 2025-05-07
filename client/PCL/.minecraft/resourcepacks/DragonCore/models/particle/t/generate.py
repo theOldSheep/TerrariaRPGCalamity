7b6cb8981ff9eac6300e2088911fd3ce461fe3c1
theOldSheep <sunnymengyangliu@gmail.com>
2025-05-06 18:27:42 -0600

    snowstorm particles & generator

--- client/PCL/.minecraft/resourcepacks/DragonCore/models/particle/t/generate.py
+++ client/PCL/.minecraft/resourcepacks/DragonCore/models/particle/t/generate.py
@@ -1,11 +1,20 @@
 import os
 
+assert os.path.isfile("generate.py")
+
+PARTICLE_DENSITY = 1
+OPACITY = 0.5
+OPACITY_HEX = "80"
+
 for filename in os.listdir():
     print(filename)
     if filename.lower().endswith(('.json')):
         print("JSON!")
         with open(filename, 'r') as f:
             lines = ''.join(f.readlines())
+            lines = lines.replace("\"max_particles\": \"", f"\"max_particles\": \"{PARTICLE_DENSITY} * ")
+            lines = lines.replace("%OPACITY%", f"{OPACITY}")
+            lines = lines.replace("%OPACITY_HEX%", f"{OPACITY_HEX}")
             dir_name = filename.replace('.json', '')
             # create folder
             try:
